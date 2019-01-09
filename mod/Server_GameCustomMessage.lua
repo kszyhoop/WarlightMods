09ed7bc70eb61c18c5255eca75d40c57316244a7
kszyhoop <jamulear@o2.pl>
2019-01-09 14:02:40 +0100

    Update Server_GameCustomMessage.lua

--- mod/Server_GameCustomMessage.lua
+++ mod/Server_GameCustomMessage.lua
@@ -1,10 +1,10 @@
 function Server_GameCustomMessage(game, playerID, payload, setReturnTable)
-	if payload.Message == "InitialPopupDisplayed" then
+	--if payload.Message == "InitialPopupDisplayed" then
 		--local playerGameData = Mod.PlayerGameData;
 		--if (playerGameData[playerID] == nil) then
 		--	playerGameData[playerID] = { };
 		--end;
 		--playerGameData[playerID].InitialPopupDisplayed = true;
 		--Mod.PlayerGameData = playerGameData;
-	end
+	--end
 end
