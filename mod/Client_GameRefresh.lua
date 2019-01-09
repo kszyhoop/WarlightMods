6e8057f2072bf08a540238c5e679ef52fddaba78
kszyhoop <jamulear@o2.pl>
2019-01-09 14:49:27 +0100

    Update Client_GameRefresh.lua

--- mod/Client_GameRefresh.lua
+++ mod/Client_GameRefresh.lua
@@ -1,12 +1,11 @@
 function Client_GameRefresh(game)
-	local payload = {};
-	payload.Message = "InitialPopupDisplayed";
-	game.SendGameCustomMessage("Please wait... ", payload, callback);
-	--if game.Game.TurnNumber > 0 then return end; -- don't pop up in ongoing games, could be removed later
+	if game.Game.TurnNumber > 0 then return end; -- don't pop up in ongoing games, could be removed later
 
-	--if not Mod.PlayerGameData.InitialPopupDisplayed then
-	--	UI.Alert("This game includes Swap Picks Mod - you pick for your opponent. Good luck :)")
-	--	UI.Alert(game.SendGameCustomMessage);
+	if not Mod.PlayerGameData.InitialPopupDisplayed then
+		UI.Alert("This game includes Swap Picks Mod - you pick for your opponent. Good luck :)")
+		local payload = {};
+		payload.Message = "InitialPopupDisplayed";
+		game.SendGameCustomMessage("Please wait... ", payload, callback);
 	--end
 end
 
