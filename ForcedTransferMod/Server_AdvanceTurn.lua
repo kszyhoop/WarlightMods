521f8b393e21e118e205ffd74fe9c434ad0a5652
kszyhoop <jamulear@o2.pl>
2019-05-27 14:34:15 +0200

    Update Server_AdvanceTurn.lua

--- ForcedTransferMod/Server_AdvanceTurn.lua
+++ ForcedTransferMod/Server_AdvanceTurn.lua
@@ -1,4 +1,5 @@
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
 	if order.proxyType ~= 'GameOrderAttackTransfer' then return end;
 	result.ActualArmies = order.NumArmies;
+	result.DefendingArmiesKilled = WL.Armies.Create(2);
 end
